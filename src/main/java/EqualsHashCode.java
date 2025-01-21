f028797a6915502170e9192199db2c4b3c92d2ae
artur <a>
2025-01-21 23:53:45 +0300

    Update Retentions.

--- src/main/java/EqualsHashCode.java
+++ src/main/java/EqualsHashCode.java
@@ -10,26 +10,23 @@ public class EqualsHashCode {
     public static void main(String[] args) {
         EqualsHashCode object1 = new EqualsHashCode(5, 10);
         EqualsHashCode object2 = new EqualsHashCode(5, 10);
-        System.out.println(object1.equals(object2));// должно быть true
+        System.out.println("1." + object1.equals(object2));// должно быть true
         int hCode;
         int hCode1;
         hCode = object1.hashCode();
         hCode1 = object2.hashCode();
-        System.out.println(hCode);
-        System.out.println(hCode1);
+        System.out.println("2." + hCode);
+        System.out.println("3." + hCode1);
 
 
         EqualsHashCode object3 = new EqualsHashCode(5, 10);
         EqualsHashCode object4 = object3;// Переменная object4 ссылается на тот-же объект что и переменная object3
-        System.out.println(object3.equals(object4));// будет true
+        System.out.println("4." + object3.equals(object4));// будет true
+
+        Object object = new Object();
+        int hCode2;
+        hCode2 = object.hashCode();
+        System.out.println("5." +hCode2);
+        //Xеш-код — это число.
     }
-
-//    public static void main(String[] args) {
-//        Object object = new Object();
-//        int hCode;
-//        hCode = object.hashCode();
-//        System.out.println(hCode);
-//    }
-    //Xеш-код — это число.
-
 }
--- src/main/java/annotation/Retentions.java
+++ src/main/java/annotation/Retentions.java
@@ -5,9 +5,38 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+//Чтобы ограничить область применения аннотации, используется аннотация @Target, которая определяет, где её можно применять:
 @Target(value= ElementType.FIELD)
+//@Target() — эта аннотация описывает, где вы можете применить свою собственную аннотацию.
+// Он принимает перечисление ElementType в качестве единственного аргумента, но можно его и не указывать.
+// Поскольку мы хотим, чтобы аннотация была действительна только для классов, мы будем использовать TYPE.
+// Кроме того, мы можем передать несколько ElementTypes, например @Target({ElementType.TYPE, ElementType.METHOD}).
 @Retention(value= RetentionPolicy.RUNTIME)
+//@Retention() — эта аннотация используется для определения того, на каком уровне аннотация будет храниться с помощью
+// политик хранения. В 99% случаев мы используем RetentionPolicy.RUNTIME. Это сохраняет аннотацию доступной на
+// протяжении всего фактического выполнения программы.
 public @interface Retentions {
     String name();
     String type();
 }
+
+//Аннотация @Target задаёт следующие области применения:
+//
+//@Target(ElementType.PACKAGE) – для пакетов;
+//@Target(ElementType.TYPE) – для классов;
+//@Target(ElementType.CONSTRUCTOR) – для конструкторов;
+//@Target(ElementType.METHOD) – для методов;
+//@Target(ElementType.FIELD) – для полей (переменных) класса;
+//@Target(ElementType.PARAMETER) – для параметров метода;
+//@Target(ElementType.LOCAL_VARIABLE) – для локальных переменных;
+//@Target(ElementType.ANNOTATION_TYPE) - для аннотирования других аннотаций, как, например, @Target и @Retention.
+
+
+//Помимо @Target, существуют и другие аннотации для настройки поведения аннотаций:
+//
+//@Retention определяет, на каком этапе жизненного цикла программы аннотация будет доступна:
+//
+//SOURCE - аннотация доступна только в исходном коде и удаляется при компиляции;
+//CLASS - аннотация сохраняется в .class файле, но недоступна во время выполнения программы;
+//RUNTIME - аннотация сохраняется в .class файле и доступна во время выполнения программы.

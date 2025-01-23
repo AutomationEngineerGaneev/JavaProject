a7cf995093ac91d9b257ff03db0b91aa55891684
artur <a>
2025-01-24 00:21:59 +0300

    API.

--- src/main/java/api_methods/APIExample.java
+++ src/main/java/api_methods/APIExample.java
@@ -24,7 +24,6 @@ public class APIExample {
                         .build();
 
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-//        System.out.println("1. " + response.body());
         return response.body();
     }
 
@@ -42,7 +41,7 @@ public class APIExample {
         responsePost.body();
     }
 
-    public String sendRequestService1() throws URISyntaxException, IOException, InterruptedException {
+    public String sendRequestService1() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -51,10 +50,21 @@ public class APIExample {
                 .build();
 
         HttpResponse<String> responsePost = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-//        System.out.println(responsePost.body());
-
         return responsePost.body();
+    }
+
+    public String sendRequestService2() throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://example.com/api")) // Адресат
+                .headers("Content-Type", "application/x-www-form-urlencoded") // Маркировка содержимого
+                .POST(HttpRequest.BodyPublishers.ofString("param1=value1&param2=value2")) // Данные для отправки
+                .build(); // Сформированный запрос
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // Получение ответа
+
+        return response.body();
     }
 
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
@@ -63,7 +73,9 @@ public class APIExample {
 
         String apiExampleSend = apiExample.sendRequestGetIsTestService();
         String apiExampleSend1 = apiExample.sendRequestService1();
+        String apiExampleSend2 = apiExample.sendRequestService2();
         System.out.println("1." + " " + apiExampleSend);
         System.out.println("2." + " " + apiExampleSend1);
+        System.out.println("3." + " " + apiExampleSend2);
     }
 }

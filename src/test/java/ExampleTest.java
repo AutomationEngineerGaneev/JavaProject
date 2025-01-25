fb110a93eddb8af84472bd216d63d7866824434e
artur <a>
2025-01-25 23:42:14 +0300

    Rest Assured.

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,15 @@
 import api_methods.base_arrays_example.User;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ExampleTest {
 
@@ -10,5 +18,19 @@ public class ExampleTest {
         User user = new User();
         System.out.println("1." + user.jsonUser());
         System.out.println("2." + user.jsonObject()); // Выводим сформированный JSON.
+    }
+
+    @Test
+    public void get(){
+        RestAssured.baseURI = "https://postman-echo.com";
+        given()
+                .param("foo1", "bar1")
+                .param("foo2", "bar2")
+                .when()
+                .get("/get")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("args.foo1", equalTo("bar1"));
     }
 }

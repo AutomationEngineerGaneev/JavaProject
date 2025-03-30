e38bd86b2b1c4913aa696a93fb925b941f695118
artur <a>
2025-03-30 13:18:20 +0300

    List added.

--- src/main/java/annotation/Retentions.java
+++ src/main/java/annotation/Retentions.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-
 //Чтобы ограничить область применения аннотации,
 // используется аннотация @Target, которая определяет, где её можно применять:
 @Target(value= ElementType.FIELD)
--- src/main/java/collections/ExampleItem.java
+++ src/main/java/collections/ExampleItem.java
@@ -1,24 +1,4 @@
 package collections;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class ExampleItem {
-
-    // Требуется реализовать отдельный метод notUnique(), который будет искать повторяющиеся слова в наборе слов.
-// Аргумент метода и возвращаемое значение должно быть типа Collection.
-// Слова изначально записаны в виде строки - требуется выполнить преобразование в коллекцию в методе main.
-// Результат вывести на экран.
-
-// Входные данные: "one two three four two five one nine three one"
-// Результат: {one, two, three}
-
-    public static void main (String[] args) {
-        String line = "one two three four two five one nine three one";
-        System.out.println(line);
-        String[] a = line.split(" ");
-        System.out.println(a);
-        List<String> list  = Arrays.asList(a);
-        System.out.println(list);
-    }
 }

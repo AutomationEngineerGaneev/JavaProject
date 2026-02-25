19f1e7b2795dc9ca0fdeb6a1ca43c76bf372009a
artur <a>
2026-02-25 23:26:39 +0300

    Added replaceAll class.

---
+++ src/main/java/replace/UpdateStr.java
@@ -0,0 +1,24 @@
+package replace;
+
+public class UpdateStr {
+
+    public static void main(String[] args) {
+        String str = "Java123 - это456 язык программирования789.";
+
+        // 1. Замена всех цифр на пустую строку (удаление цифр)
+        String noDigits = str.replaceAll("\\d", "");
+        System.out.println(noDigits); // "Java - это язык программирования."
+
+        // 2. Замена слова
+        String replacedWord = str.replaceAll("Java", "Kotlin");
+        System.out.println(replacedWord); // "Kotlin123 - это456 язык программирования789."
+
+        // 3. Замена слова
+        String strSecond = "Неразрывный пробел-&nbsp;-штука необычная";
+
+        String replacedWord1 = strSecond.replaceAll("&nbsp;", " ");
+        String replacedWord2 = strSecond.replaceAll("&nbsp;", "");
+        System.out.println(replacedWord1); // "Неразрывный пробел- -штука необычная"
+        System.out.println(replacedWord2); // "Неразрывный пробел--штука необычная"
+    }
+}

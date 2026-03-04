28de37aff73dc4bde0c4c1d5cdd6e57e219ba550
artur <a>
2026-03-04 21:19:49 +0300

    Added replaceAll class.

--- src/main/java/replace/UpdateStr.java
+++ src/main/java/replace/UpdateStr.java
@@ -20,5 +20,13 @@ public class UpdateStr {
         String replacedWord2 = strSecond.replaceAll("&nbsp;", "");
         System.out.println(replacedWord1); // "Неразрывный пробел- -штука необычная"
         System.out.println(replacedWord2); // "Неразрывный пробел--штука необычная"
+
+        String fieldValueFirst = "<br> 06.11.2020 в 21:43 после включения выключателей 330 кВ блочного трансформатора 2Т обнаружено отсутствие тока в фазе «С» на выключателе В-1 ГТ-2 и пропадание на АРМ телеизмерений тока фазы «С» с обоих выключателей.";
+        String fieldValue = fieldValueFirst.trim().replaceAll("(?i)<br\\s*/?>", "");
+            System.out.println("Field value: " + fieldValue);
+
+        if(fieldValue.contains("06.11.2020 в")){
+            System.out.println("Field value2: " + fieldValue);
+        }
     }
 }
